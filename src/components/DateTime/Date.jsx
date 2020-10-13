2b15550fdad5d82f27bf09a8169e3bf60652f89d
jhonatan lopez <55928962+jhonatanlopez13@users.noreply.github.com>
2020-10-13 09:10:46 -0500

    datetime funcional

--- src/components/DateTime/Date.jsx
+++ src/components/DateTime/Date.jsx
@@ -1,13 +1,15 @@
 import React, { Component} from "react";
-import DatePicker from "react-datepicker";
+import DatePicker  from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./module.css/style.css";
+
+
 
 class DateTime extends Component{
   state={
     fecha:new Date()
   }
-  onChanger=fecha=>{
+  onChange=fecha=>{
     this.setState({fecha: fecha});
   }
   mostrarFecha = fecha=>{
@@ -21,7 +23,7 @@ class DateTime extends Component{
       <div className="container">
         <div className="calendario1">
           <div className="almanaque" >
-            <DatePicker className="border-gray border-2 rounded px-6 py-1"  selected={this.state.fecha}onChange={this.onChange}/>
+            <DatePicker className="border-gray border-2 rounded px-6 py-1"  selected={this.state.fecha} onChange={this.onChange} />
             <button className="boton  icon-ic_calendar " onClick={()=>this.mostrarFecha(this.state.fecha)}>  </button>
         </div>
         </div>
--- src/components/Help/Help.jsx
+++ src/components/Help/Help.jsx
@@ -10,7 +10,7 @@ class Help extends Component{
                <div className></div>
                 <div class="tooltip">
                     <h1 className="ayuda"></h1>
-                <span class="tooltiptext">Tooltip text</span>
+                <span class="tooltiptext">colocar mensaje de ayuda </span>
                 </div>
 
             </>
