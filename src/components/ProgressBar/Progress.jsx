f681ca7403a3b5ca782e5a33028b4b064acdd5c2
jhonatan lopez <55928962+jhonatanlopez13@users.noreply.github.com>
2020-10-14 16:45:17 -0500

    ok

--- src/components/ProgressBar/Progress.jsx
+++ src/components/ProgressBar/Progress.jsx
@@ -7,15 +7,12 @@ class Progress extends Component{
     
     render(){
         return(
-          <div className="container">
-              <div className="relative pt-1">
-                        <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-yellow-200">
-                            <div style={{ width: "10%" }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"></div>
-                            <div style={{ width: "15%" }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-orange-500"></div>
-                            <div style={{ width: "25%" }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-yellow-500"></div>
-                        </div>
-                    </div>
+          <div className="contenedorProgressBar">
+            <span className="progresstext"></span>
+            <progress value="50" max="100">30%</progress>
+              {/* <progress max="100">Indeterminate</progress> */}
           </div>
+         
            
         )
     }
