17b85e7a6ef0d0a0ba1e257574708dc40d9a3120
jhonatan lopez <55928962+jhonatanlopez13@users.noreply.github.com>
2020-10-20 11:29:58 -0500

    ok

--- src/components/ProgressBar/Progress.jsx
+++ src/components/ProgressBar/Progress.jsx
@@ -7,14 +7,10 @@ class Progress extends Component{
     
     render(){
         return(
-          <div className="contenedorProgressBar">
-            <span className="progresstext"></span>
-            <progress value="50" max="100">30%</progress>
-              {/* <progress max="100">Indeterminate</progress> */}
-          </div>
+         <div className="progress-bar"></div>
          
            
         )
     }
 }
-export default Progress;
+export default funtion Progress ({anch});
