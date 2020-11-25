de65332b5da0f50ec761ab107865ad0cd5c460ef
jhonatan lopez <55928962+jhonatanlopez13@users.noreply.github.com>
2020-11-25 08:42:22 -0500

    skeleton completo

--- src/components/Esqueleton/Esqueleton.jsx
+++ src/components/Esqueleton/Esqueleton.jsx
@@ -5,31 +5,31 @@ class Esqueleton extends Component{
     render(){
         return(
             <>
-                <div className="skeleton-wrapper">
-                    <div className="skeleton">
-                        <div className="skeleton-incicator"></div>
+                <div className="skeleton-wrapper animate-pulse">
+                    <div className="skeleton animate-pulse">
+                        {/* <div className="skeleton-incicator animate-pulse"></div> */}
                     </div>                    
                 </div>
 
 
-                <div className="skeleton-wrapper1">
-                    <div className="skeleton1">
-                        <div className="skeleton-incicator1"></div>
+                <div className="skeleton-wrapper1 animate-pulse">
+                    <div className="skeleton1 animate-pulse">
+                        {/* <div className="skeleton-incicator1 animate-pulse"></div> */}
                     </div>                    
                 </div>
 
 
-                <div className="skeleton-wrapper1-2">
-                    <div className="skeleton1-2">
-                        <div className="skeleton-incicator1-2"></div>
+                <div className="skeleton-wrapper1-2 animate-pulse">
+                    <div className="skeleton1-2 animate-pulse">
+                        {/* <div className="skeleton-incicator1-2 animate-pulse"></div> */}
                     </div>                    
                 </div>
 
 
 
-                <div className="skeleton-wrapper1-3">
-                    <div className="skeleton1-3">
-                        <div className="skeleton-incicator1-3"></div>
+                <div className="skeleton-wrapper1-3 animate-pulse">
+                    <div className="skeleton1-3 animate-pulse">
+                        {/* <div className="skeleton-incicator1-3 animate-pulse"></div> */}
                     </div>                    
                 </div>
 
@@ -37,51 +37,56 @@ class Esqueleton extends Component{
 
        
 
-                <div className="skeleton-wrapper1-4">
-                    <div className="skeleton1-4">
-                        <div className="skeleton-incicator1-4"></div>
+                <div className="skeleton-wrapper1-4 animate-pulse">
+                    <div className="skeleton1-4 animate-pulse">
+                        {/* <div className="skeleton-incicator1-4 animate-pulse"></div> */}
                     </div>                    
                 </div>
 
                
 
         
-            <div className="skeleton-wrapper2">
-                    <div className="skeleton2">
-                        <div className="skeleton-incicator2"></div>
+            <div className="skeleton-wrapper2 animate-pulse">
+                    <div className="skeleton2 animate-pulse">
+                        {/* <div className="skeleton-incicator2 animate-pulse"></div> */}
                     </div>                    
              </div>
       
       
 
-             <div className="skeleton-wrapper3">
-                    <div className="skeleton3">
-                        <div className="skeleton-incicator3"></div>
+             <div className="skeleton-wrapper3 animate-pulse">
+                    <div className="skeleton3 animate-pulse">
+                        {/* <div className="skeleton-incicator3 animate-pulse"></div> */}
                     </div>                    
              </div>
       
                 
 
-             <div className="skeleton-wrapper4">
-                    <div className="skeleton4">
-                        <div className="skeleton-incicator4"></div>
+             <div className="skeleton-wrapper4 animate-pulse">
+                    <div className="skeleton4 animate-pulse">
+                        {/* <div className="skeleton-incicator4 animate-pulse"></div> */}
                     </div>                    
             </div>
 
 
-            <div className="skeleton-wrapper5">
-                    <div className="skeleton5">
-                        <div className="skeleton-incicator5"></div>
+            <div className="skeleton-wrapper5 animate-pulse">
+                    <div className="skeleton5 animate-pulse">
+                        {/* <div className="skeleton-incicator5 animate-pulse"></div> */}
                     </div>                    
             </div>
 
-            <div className="skeleton-wrapper6">
-                    <div className="skeleton6">
-                        <div className="skeleton-incicator6"></div>
+            <div className="skeleton-wrapper6 animate-pulse">
+                    <div className="skeleton6 animate-pulse">
+                        {/* <div className="skeleton-incicator6 animate-pulse"></div> */}
                     </div>                    
             </div>
            
-
+            <div className="skeleton-wrapper7 animate-pulse">
+                    <div className="skeleton7 animate-pulse">
+                        {/* <div className="skeleton-incicator6 animate-pulse"></div> */}
+                    </div>                    
+            </div>
+           
             </>
         )
     }
